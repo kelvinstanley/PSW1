afe61801ef4f14cc7558ad665983236f30b7cfd9
Kelvin <kelvingbi47@gmail.com>
2024-11-13 16:55:35 -0300

    atividade13-11

---
+++ aula-13-11/index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>aula-13-11</title>
+    <link rel="stylesheet" href="style.css">
+<body>
+    <div class="navegacao">
+        <ul>
+            <li><a href="https://www.w3schools.com">W3Schools</a></li>
+            <li><a href="https://ceteia.guanambi.ifbaiano.edu.br/pswmedio/2023/2ai/buffet/src/index.php">Buffet</a></li>
+            <li><a href="https://ceteia.guanambi.ifbaiano.edu.br/pswmedio/2023/2ai/cineview/cineview/index.php">Cineview</a></li>
+            <li><a href="https://ceteia.guanambi.ifbaiano.edu.br/pswmedio/2023/2ai/livraria/">Livraria</a></li>
+            <li><a href="https://ceteia.guanambi.ifbaiano.edu.br/pswmedio/2023/2ai/neurobiblioteca/">Neurobiblioteca</a></li>
+        </ul>
+    </div>
+</body>
+</html>
